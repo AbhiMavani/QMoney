fc552a23d5d1d3e33fa3bfd757cc9cdfbea14c70
Abhishek Mavani <52848980+AbhiMavani@users.noreply.github.com>
2020-07-23 12:32:13 +0530

    Add files via upload

---
+++ tingo_curl.sh
@@ -0,0 +1,3 @@
+curl https://api.tiingo.com/tiingo/daily/GOOGL/prices?token=6d7fd8b1cb1df3582780c7017db377f977642d71
+
+curl https://api.tiingo.com/tiingo/daily/AAPL/prices?token=6d7fd8b1cb1df3582780c7017db377f977642d71
